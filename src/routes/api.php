a21d6eecc1417c163a45bf8cddf5bde55d428c06
Meysam Maghsoudi <safir.1987@gmail.com>
2022-04-14 16:46:20 +0430

    fix bug route namespace

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/todo')->namespace('meysammaghsoudi\Todopackage\Http\Controllers\Api\v1')->group(function() {
     Route::get('/tasks', 'ApiController@getTasks');
+    
 });
